264e7894e72cbaeb47dd6a4ffb32f7c70066f237
PanaceaBathory <zikoar@protonmail.com>
2022-05-01 18:23:12 -0300

    Various Things

--- thingy/src/App.js
+++ thingy/src/App.js
@@ -1,35 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
 
 const DEFAULT_TRANSACTIONS = [
 
 ]
-function TransactTable({ transactions, onDelete }) {
-
+const DEFAULT_MODAL = {
+  state: false,
+  id: ''
+}
+function TransactTable({ transactions, onDelete, onModal }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Description</th>
-          <th>Amount</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map(t => (
-          <tr key={t.id}>
-            <td>{t.id}</td>
-            <td>{t.description}</td>
-            <td>{t.amount}</td>
-            <td><button onClick={() => {
-              onDelete(t.id)
-            }}>Delete</button></td>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Delete</th>
+            <th>Edit</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {transactions.map(t => (
+            <tr key={t.id}>
+              <td>{t.id}</td>
+              <td>{t.description}</td>
+              <td>{t.amount}</td>
+              <td><button onClick={() => {
+                onDelete(t.id)
+              }}>Delete</button></td>
+              <td><button onClick={() => {
+                onModal({ id: t.id, state: true })
+              }}>Edit</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+    </div>
   )
 }
 class TransactForm extends React.Component {
@@ -69,9 +97,45 @@ class TransactForm extends React.Component {
     )
   }
 }
+function EditModal({ info, setModal, updateTable }) {
+  const editFetch = React.useCallback((t) => {
+    fetch('http://127.0.0.1:5000/' + info.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+
+      },
+      body: JSON.stringify(t)
+    })
+      .then(res => res.json())
+      .then(data => updateTable(data))
+      .then(() => {
+        setModal({ id: '', state: false })
+      })
+  }, [setModal, info.id])
+
+
+  return (
+    <div>
+      <Modal
+        open={info.state}
+        onClose={() => {
+          setModal({ id: '', state: false })
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <TransactForm onSubmit={editFetch} />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
 
 function Account() {
   const [transactions, setTransactions] = React.useState(DEFAULT_TRANSACTIONS)
+  const [modalState, setModalState] = React.useState(DEFAULT_MODAL)
   const total = React.useMemo(() => transactions.reduce((a, b) => a + b.amount, 0), [transactions])
   React.useEffect(() => {
     fetch('http://127.0.0.1:5000/')
@@ -107,11 +171,24 @@ function Account() {
         setTransactions(transactions.filter((e) => { return e.id !== t }))
       })
   }, [transactions])
+  const modalMod = React.useCallback((t) => {
+    setModalState(t)
+  }, [])
+
+  const updateEntries = React.useCallback((t) => {
+    const ntransaction = [...transactions]
+    const index = transactions.findIndex((e) =>
+      e.id === t.id
+    )
+    ntransaction[index] = t
+    setTransactions(ntransaction)
+  })
   return (
     <div>
       Total: {total}
-      <TransactTable transactions={transactions} onDelete={removeTransaction} />
+      <TransactTable transactions={transactions} onDelete={removeTransaction} onModal={modalMod} />
       <TransactForm onSubmit={addTransaction} />
+      <EditModal info={modalState} setModal={modalMod} updateTable={updateEntries} />
     </div>
   )
 }
